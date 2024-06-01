62eb0dd907d6bf7492d272b1bf84e70c726e7220
Pedro Henrique <pedro2266809ph@gmail.com>
2024-06-01 12:53:19 -0300

    first commit

---
+++ src/test/java/io/github/pedrohss2/msAvaliadorDeCreditos/MsavaliadordecreditosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pedrohss2.msAvaliadorDeCreditos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsavaliadordecreditosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
